60839fc52240ee7585cfffb07b88b524107a5bc8
Castellani-Josue <josue.castellani@gmail.com>
2025-09-27 18:41:28 +0200

    Initial commit

---
+++ src/main/java/com/baeldung/algorithms/RunAlgorithm.java
@@ -0,0 +1,11 @@
+package com.baeldung.algorithms;
+
+import com.baeldung.algorithms.ga.binary.SimpleGeneticAlgorithm;
+
+public class RunAlgorithm {
+
+	public static void main(String[] args) {
+		SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm();
+		ga.runAlgorithm(50, "1011000100000100010000100000100111001000000100000100000000001111");
+	}
+}
